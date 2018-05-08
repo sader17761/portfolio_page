232dc298e200994ad6125ab401056f3f17831f7a
Corey <corey.sader@gmail.com>
2018-05-08 15:42:27 -0500

    added prev/next functionality

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// this code will run when the document is ready...
+$(document).ready(function() {
+    // instructions on how to use 'superslides' is here: https://github.com/nicinabox/superslides
+    $('#slides').superslides({
+        animation: 'fade',
+        play: 5000,
+        pagination: false
+    });
+});
+
